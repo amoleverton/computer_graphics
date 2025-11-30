84b2e978245c08218b4a7fea772ea36fbfc28c36
amoleverton <123089336+amoleverton@users.noreply.github.com>
2025-11-30 17:22:06 +0530

    Add files via upload

---
+++ P17.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<graphics.h>
+#include<conio.h>
+void boundary_fill(int x,int y,int bcolor,int fcolor);
+void main()
+{
+ int gd=DETECT,gm;
+ initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
+ setcolor(WHITE);
+ line(200,200,300,300);
+ line(300,300,220,470);
+ line(220,470,170,470);
+ line(170,470,100,300);
+ line(100,300,200,200);
+ boundary_fill(200,300,WHITE,YELLOW);
+ getch();
+ closegraph();
+}
+
+void boundary_fill(int x, int y, int bcolor, int fcolor)
+{
+ if(getpixel(x,y)!=bcolor)
+ {
+  delay(50);
+  putpixel(x,y,fcolor);
+  boundary_fill(x,y+1,bcolor,fcolor);
+  boundary_fill(x+1,y,bcolor,fcolor);
+  boundary_fill(x,y-1,bcolor,fcolor);
+  boundary_fill(x-1,y,bcolor,fcolor);
+  }
+}
